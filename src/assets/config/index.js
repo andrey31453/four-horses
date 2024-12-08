dfbf0b7047d90d74a7752131ba34b46626c6498b
andrey13453 <andrey13453@gmail.com>
2024-12-08 23:58:41 +0300

    init

---
+++ src/assets/config/index.js
@@ -0,0 +1,47 @@
+export const config = {
+	screens: {
+		sm: 640,
+		md: 768,
+		lg: 1024,
+		xl: 1280,
+	},
+	indent: {
+		sizes: 20,
+	},
+	grid: {
+		cols: 12,
+	},
+
+	fontSizes: {
+		xs: 16,
+		sm: 18,
+		base: 20,
+		lg: 22,
+		xl: 24,
+		'2xl': 36,
+		'3xl': 54,
+		'4xl': 60,
+	},
+
+	colors: {
+		white: '#ffffff',
+		black: '#313131',
+		red: '#f54932',
+		blue: '#3057a2',
+
+		primary: '#f54932',
+
+		'surface-200': '#585653',
+		'surface-300': '#6f6f6f',
+		'surface-400': '#7b7672',
+		'surface-500': '#838383',
+		'surface-600': '#8b8b8b',
+		'surface-700': '#d0d0d0',
+		'surface-800': '#e9ded4',
+
+		// btn vars
+		btn: '#313131',
+		'btn-hover': '#fbce51',
+		'btn-disabled': '#d6d6d6',
+	},
+}
