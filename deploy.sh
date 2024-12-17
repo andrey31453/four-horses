dcac748ee6545d1a754ffb43b3828e4bf3bf13e0
andrey13453 <andrey13453@gmail.com>
2024-12-17 21:18:36 +0300

    dist

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 set -e
-npm run build
+pnpm run build
 cd dist
 
-#git init
+git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:andrey31453/four-horses.git main:gh-pages
+git push -f git@github.com:andrey31453/four-horses.git master:gh-pages
 cd -
